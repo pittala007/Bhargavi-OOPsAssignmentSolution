638f8c1ca57b3fbb164cf9994e436ec8633cf4c0
pittala007 <89726059+pittala007@users.noreply.github.com>
2021-09-20 05:37:27 -0700

    Add files via upload

---
+++ HrDepartment.java
@@ -0,0 +1,19 @@
+package com.greatLearning.assignment;
+
+public class HrDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "Welcome to HR department";
+				
+				
+	}
+	public String getTodaysWork() {
+		return "fill todays timesheet and mark your attendance";
+	}
+	public String doActivity() {
+		return "team Lunch";
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+}
---
+++ SuperDepartment.java
@@ -0,0 +1,17 @@
+package com.greatLearning.assignment;
+
+public class SuperDepartment {
+	public String departmentName() {
+		return "Super Department";
+	}
+	public String getTodayWork() {
+		return "No work as of now";
+	}
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+	public String isTodayAHoliday() {
+		return "Today is not a Holiday";
+	}
+
+}
